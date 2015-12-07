2ff8cc2e1e77718d7c8005f99a069389010bec9e
Luda Zhao <94luda@gmail.com>
2015-12-07 10:33:57 -0800

    traffic modeling

--- scripts/traffic_modeling/main.py
+++ scripts/traffic_modeling/main.py
@@ -11,20 +11,35 @@ edge_speed = {}
 nd_to_e = {}
 # maps from the node idea to its coordinates
 nodes = {}
+graph = {}
 
 for line in e_file:
-	ids = line.rstrip().split()
-	edge_typo[(int(ids[0]), int(ids[1]))] = ids[3]
-	edge_speed[(int(ids[0]), int(ids[1]))] = ids[4]
-	nd_to_e[(int(ids[0]), int(ids[1]))] = ids[2]
+    ids = line.rstrip().split()
+    if ids[0] == ids[1]:
+        continue
+    if (int(ids[0]), int(ids[1])) == (14282, 10113):
+        continue
+    e1 = int(ids[0])
+    e2 = int(ids[1])
+    if e1 not in graph:
+        graph[e1] = []
+    if e2 not in graph:
+        graph[e2] = []
+
+    graph[e1].append(e2)
+    graph[e2].append(e1)
+
+    edge_typo[(int(ids[0]), int(ids[1]))] = ids[3]
+    edge_typo[(int(ids[1]), int(ids[0]))] = ids[3]
+    edge_speed[(int(ids[0]), int(ids[1]))] = ids[4]
+    edge_speed[(int(ids[1]), int(ids[0]))] = ids[4]
+    nd_to_e[(int(ids[0]), int(ids[1]))] = ids[2]
+    nd_to_e[(int(ids[1]), int(ids[0]))] = ids[2]
 
 for line in n_file:
-	data = line.rstrip().split()
-	nodes[int(data[0])] = (float(data[1]), float(data[2]))
+    data = line.rstrip().split()
+    nodes[int(data[0])] = (float(data[1]), float(data[2]))
 
 G = snap.LoadEdgeList(snap.PUNGraph, '../n_sf_edges.txt', 0, 1)
-
-TrafficModel(G, nodes, edge_typo, edge_speed, nd_to_e)
-
-
-
+model = TrafficModel(G,graph, nodes, edge_typo, edge_speed, nd_to_e)
+model.iterate(10, 1000)
--- scripts/traffic_modeling/traffic_model.py
+++ scripts/traffic_modeling/traffic_model.py
@@ -1,6 +1,9 @@
 import snap
+import Queue
+import copy
 import math
 from geopy.distance import vincenty
+import random
 
 #Elena did this
 #def getDistance(coor1, coor2):
@@ -12,12 +15,16 @@ class TrafficModel:
 
     # graph: snappy graph, node_attr: a dict of n1 to node coordinates(lat, long), edge_typo, edge_speed: a dict of (n1, n2) to edge properties, n_to_e: a link from (n1, n2) to a list of edge ids(to preserve which edges are part of the same road)
     # the only changing variables to calculate our trafficmodel is self.e_flow and self.e_time. All other variables are constant
-    def __init__(self, graph, node_attr, edge_typo, edge_speed, n_to_e):
-        self.graph = graph
+    def __init__(self, graph, better_graph, node_attr, edge_typo, edge_speed, n_to_e):
+        self.graph2 = graph
+        self.g = better_graph
         self.node_coor = node_attr #the coordinate of each node
         self.e_type = edge_typo #the typology of each edge
         self.e_dist = {} #the distance for each edge
         self.e_speed = edge_speed # the maximum allowed speed of each edge.
+        self.num_nodes = graph.GetNodes()
+        self.capacity = {}
+        print self.num_nodes
 
         #get maximum speed for all edge
         #residential: 15
@@ -25,19 +32,37 @@ class TrafficModel:
             e_type = self.e_type[e]
             if self.e_speed[e] == 'N/A':
                 if e_type == 'residential':
+                    self.capacity[e] = 1
                     self.e_speed[e] = 20
                 elif e_type == 'tertiary':
+                    self.capacity[e] = 2
                     self.e_speed[e] = 25
                 elif e_type == 'secondary':
+                    self.capacity[e] = 2
                     self.e_speed[e] = 25
                 elif e_type == 'primary':
+                    self.capacity[e] = 3
                     self.e_speed[e] = 35
-                elif e_type == 'motorway':
+                elif e_type == 'motorway' or e_type == 'trunk':
+                    self.capacity[e] = 4
                     self.e_speed[e] = 55
                 else:
+                    self.capacity[e] = 1
                     self.e_speed[e] = 25
             else:
-              speed = int(self.e_speed[e])
+                if e_type == 'residential':
+                    self.capacity[e] = 1
+                elif e_type == 'tertiary':
+                    self.capacity[e] = 2
+                elif e_type == 'secondary':
+                    self.capacity[e] = 2
+                elif e_type == 'primary':
+                    self.capacity[e] = 3
+                elif e_type == 'motorway' or e_type == 'trunk':
+                    self.capacity[e] = 4
+                else:
+                    self.capacity[e] = 1
+                self.e_speed[e] = int(self.e_speed[e].replace("mph", ""))
 
         #calculate distance for each edge(loop over e_type since it contains all edges)
         for e in self.e_type:
@@ -53,7 +78,120 @@ class TrafficModel:
             self.e_flow[(int(e[0]), int(e[1]))] = 0
 
         self.e_time = {} #the time needed to traverse an edge. Used as the weight in our Dijkstras
-        for e in self.e_time:
+        for e in self.e_type:
             self.e_time[e] = self.e_dist[e] * 60 / float(self.e_speed[e]) # in minutes
 
-        print self.e_time
+    def dijkstra(self, pair):
+        path = [0, [pair[0]]]
+        q = Queue.PriorityQueue()
+        used = set()
+        start = pair[0]
+        end = pair[1]
+
+        while start != end:
+            if start not in used:
+                used.add(start)
+                for node in self.g[start]:
+                    if node not in used:
+                        new_list = list(path[1])
+                        new_list.append(node)
+                        new_path = [path[0]+self.e_time[(start, node)], new_list]
+                        q.put(tuple(new_path))
+            if (q.empty()):
+                return -1, []
+            path = list(q.get())
+            start = path[1][-1]
+
+        return path[0], path[1]
+
+    def iterate(self, numIter, numTest):
+
+        test_pairs = {}
+        #Generate the random pairs of start-end locations
+        for i in range(numTest):
+            pair = (random.randint(0, self.num_nodes), random.randint(0, self.num_nodes))
+            if pair[0] not in self.g:
+                continue
+            test_pairs[pair] = ()
+
+        not_feasible = set()
+        old_test_pairs = {}
+        for i in range(numIter):
+            print "Iteration ", i
+            old_test_pairs = dict(test_pairs)
+            #reset flow
+            self.e_flow = {}
+            for e in self.e_type:
+                self.e_flow[(int(e[0]), int(e[1]))] = 0
+            #go through the number of iterations
+            for j, pair in enumerate(test_pairs):
+                if pair in not_feasible:
+                    continue
+                #Associate our weights(time) with graph
+                #Find shorteset path between all pairs
+                time, path = self.dijkstra(pair)
+                test_pairs[pair] = (time, path)
+                if time == -1:
+                    not_feasible.add(pair)
+                    continue
+                for k in range(len(path) - 1):
+                    self.e_flow[(path[k], path[k+1])]+=1
+                    self.e_flow[(path[k+1], path[k])]+=1
+
+                    for e in [(path[k], path[k+1]), (path[k+1], path[k])]:
+                        self.e_time[e] = self.e_dist[e] * 60 / float(self.e_speed[e]) # in minutes
+                        if self.e_type[e] != "motorway" and self.e_type[e] != "trunk":
+                            self.e_time[e] += 18.1/60 #MAGIC PART 2
+                        self.e_time[e] *= (1 + 0.2 * pow(self.e_flow[e]*50 / (self.capacity[e] * 1400.0/60 * self.e_dist[e] * 5280), 3)) #MAGIC
+                        #print self.e_flow[e] * 500 / (self.capacity[e] * 1400.0/60 * self.e_dist[e] * 5280)
+                        #print 1+ 0.2 * pow(self.e_flow[e] * 500 / (self.capacity[e] * 1400.0/60 * self.e_dist[e] * 5280), 2)
+
+
+
+            #for e in self.e_type:
+            #    self.e_time[e] = self.e_dist[e] * 60 / float(self.e_speed[e]) # in minutes
+            #    if self.e_type[e] != "motorway" and self.e_type[e] != "trunk":
+            #        self.e_time[e] += 18.1/60 #MAGIC PART 2
+            #    self.e_time[e] *= (1 + 0.2 * pow(self.e_flow[e]*2000 / (self.capacity[e] * 1400.0/60 * self.e_dist[e] * 5280), 2)) #MAGIC
+            #   #print self.e_flow[e] * 500 / (self.capacity[e] * 1400.0/60 * self.e_dist[e] * 5280)
+            #    print 1+ 0.2 * pow(self.e_flow[e] * 500 / (self.capacity[e] * 1400.0/60 * self.e_dist[e] * 5280), 2)
+
+            #print self.e_flow
+            print max(self.e_flow, key=self.e_flow.get)
+            print max(self.e_flow.values())
+            print max(self.e_time, key=self.e_time.get)
+            print max(self.e_time.values())
+            change_arr = []
+
+            change = 0
+            if i != 0:
+                for j, pair in enumerate(old_test_pairs):
+                    old_time, old_path = old_test_pairs[pair]
+                    time, path = test_pairs[pair]
+                    if path != old_path:
+                        if j == 0:
+                            print old_path
+                            print path
+                        change+=1
+                print "Percentage change: ", change/float(len(old_test_pairs) - len(not_feasible))
+
+                #Check how many paths changed to determine if terminate
+                print change
+                percent_change = change/float(len(old_test_pairs) - len(not_feasible))
+                change_arr.append(percent_change)
+                if percent_change < 0.05:
+                    break
+                f2 = open("final_path_base_map" + str(i) + ".txt", 'w')
+                for p in test_pairs:
+                    for n in test_pairs[p][1]:
+                        f2.write("%s " % str(n))
+                    f2.write('\n')
+        print change_arr
+        f = open("edge_with_traffic_model.txt", 'w')
+        for e in self.e_time:
+            f.write( str(e[0]) +' '+ str(e[1]) + ' ' + str(self.e_time[e]) + '\n')
+            #Profit
+
+            #Profit
+
+
